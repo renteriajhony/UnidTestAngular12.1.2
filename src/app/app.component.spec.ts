55c6d3325d0d4bad17dacaf2d1f45e9931fc38b1
Jhony Renteria Rodriguez <jrenteria@personalsoft.com.co>
2021-07-23 08:30:18 -0500

    primer commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,24 +14,115 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+
+  describe('Objetos de la vista HTMl', () => {
+    it('Dentro del toolbar existe un espan con el texto \'Welcome\'', async() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.toolbar span')?.textContent).toContain('Welcome');
+    });
+
+    it('Dentro del contenido existe un espan con el texto \'Hola mundo\'', async() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('.content span')?.textContent).toContain('Hola mundo');
+    });
   });
 
-  it(`should have as title 'unitTest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('unitTest');
+  describe('Operaciones Matematicas', () => {
+    it('Suma de dos numeros, la suma debe ser 4', async () => {
+      expect(app.sumar(2, 2)).toEqual(4);
+    });
+    it('Suma de dos numeros, la suma debe ser 5', async () => {
+      expect(app.sumar(5, 0)).toEqual(5);
+    });
+    it('Resta de dos numeros, la resta debe ser 3', async () => {
+      expect(app.restar(100, 97)).toEqual(3);
+    });
+    it('Resta de dos numeros, la suma debe ser 0', async () => {
+      expect(app.restar(5, 5)).toEqual(0);
+    });
+    it('Multiplicacion de dos numeros, la multiplicacion debe ser 8', async () => {
+      expect(app.multiplicar(4, 2)).toEqual(8);
+    });
+    it('Multiplicacion de dos numeros, la multiplicacion debe ser 0', async () => {
+      expect(app.multiplicar(0, 5)).toEqual(0);
+    });
+    it('Divicion de dos numeros, la Divicion debe ser 50', async () => {
+      expect(app.dividir(100, 2)).toEqual(50);
+    });
+    it('Divicion de dos numeros, la Divicion debe ser 0', async () => {
+      expect(app.dividir(123, 0)).toEqual(0);
+    });
+    it('Divicion de dos numeros, la Divicion debe ser 0', async () => {
+      expect(app.dividir(123, 0)).toEqual(0);
+    });
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('unitTest app is running!');
+  describe('Imprimir resultados de operaciones matematicas', () => {
+    it('Imprimir suma', async () => {
+      const hostElement = fixture.nativeElement;
+      const number_a: HTMLInputElement = hostElement.querySelector('#number_a');
+      const number_b: HTMLInputElement = hostElement.querySelector('#number_b');
+      fixture.detectChanges();
+      number_a.value = '3';
+      number_b.value = '3';
+      document.getElementById('btn_s')?.click();
+      expect((<HTMLInputElement>document.getElementById('result')).value).toBe('6');
+    });
+
+    it('Imprimir resta', async () => {
+      const hostElement = fixture.nativeElement;
+      const number_a: HTMLInputElement = hostElement.querySelector('#number_a');
+      const number_b: HTMLInputElement = hostElement.querySelector('#number_b');
+      fixture.detectChanges();
+      number_a.value = '3';
+      number_b.value = '3';
+      document.getElementById('btn_r')?.click();
+      expect((<HTMLInputElement>document.getElementById('result')).value).toBe('0');
+    });
+
+    it('Imprimir multiplicacion', async () => {
+      const hostElement = fixture.nativeElement;
+      const number_a: HTMLInputElement = hostElement.querySelector('#number_a');
+      const number_b: HTMLInputElement = hostElement.querySelector('#number_b');
+      fixture.detectChanges();
+      number_a.value = '3';
+      number_b.value = '3';
+      document.getElementById('btn_m')?.click();
+      expect((<HTMLInputElement>document.getElementById('result')).value).toBe('9');
+    });
+
+    it('Imprimir divicion', async () => {
+      const hostElement = fixture.nativeElement;
+      const number_a: HTMLInputElement = hostElement.querySelector('#number_a');
+      const number_b: HTMLInputElement = hostElement.querySelector('#number_b');
+      fixture.detectChanges();
+      number_a.value = '3';
+      number_b.value = '3';
+      document.getElementById('btn_d')?.click();
+      expect((<HTMLInputElement>document.getElementById('result')).value).toBe('1');
+    });
+
+    it('Imprimir divicion cero', async () => {
+      const hostElement = fixture.nativeElement;
+      const number_a: HTMLInputElement = hostElement.querySelector('#number_a');
+      const number_b: HTMLInputElement = hostElement.querySelector('#number_b');
+      fixture.detectChanges();
+      number_a.value = '3';
+      number_b.value = '0';
+      document.getElementById('btn_d')?.click();
+      expect((<HTMLInputElement>document.getElementById('result')).value).toBe('0');
+    });
+
   });
+
+
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  //exports: [UserComponent],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
